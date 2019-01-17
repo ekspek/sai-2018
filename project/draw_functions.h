4b60946fe18b348d12070f47656275503a070199
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-01-17 00:51:22 +0000

    Added initial project files

---
+++ project/draw_functions.h
@@ -0,0 +1,23 @@
+/*-------------------------------------------------------+
+|           SAI - Trabalho Experimental                  |
++--------------------------------------------------------+
+|           Grupo 11 - Projecto E2                       |
+|                                                        |
+|Pedro Afonso                                            |
+|João Manito                                             |
+|Daniel Schiffart                                        |
++--------------------------------------------------------+
+|Protótipos das funções para gerar os diversos           |
+|indicadores da aeronave                                 |
++-------------------------------------------------------*/
+
+#ifndef __DRAW_FUNCTIONS_H__
+#define __DRAW_FUNCTIONS_H__
+
+void draw_artificial_horizon(float pitch);
+void draw_airspeed_indicator();
+void draw_heading_indicator();
+void draw_altitude_indicator();
+void draw_test();
+
+#endif
---
+++ project/global_variables.h
@@ -0,0 +1,21 @@
+/*-------------------------------------------------------+
+|           SAI - Trabalho Experimental                  |
++--------------------------------------------------------+
+|           Grupo 11 - Projecto E2                       |
+|                                                        |
+|Pedro Afonso                                            |
+|João Manito                                             |
+|Daniel Schiffart                                        |
++--------------------------------------------------------+
+|Protótipos das funções para gerar os diversos           |
+|indicadores da aeronave                                 |
++-------------------------------------------------------*/
+
+#ifndef __GLOBAL_VARIABLES_H__
+#define __GLOBAL_VARIABLES_H__
+
+# define WINDOW_SIZE_X 800
+# define WINDOW_SIZE_Y 800
+# define PI 3.14159
+
+#endif
