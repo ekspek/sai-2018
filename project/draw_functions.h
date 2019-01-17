a0d45954e5f417bfb399ccc5706a3f6283987b02
jonythunder <joaomanito1@gmail.com>
2019-01-17 01:08:24 +0000

    Add files via upload

--- project/draw_functions.h
+++ project/draw_functions.h
@@ -14,7 +14,7 @@
 #ifndef __DRAW_FUNCTIONS_H__
 #define __DRAW_FUNCTIONS_H__
 
-void draw_artificial_horizon(float pitch);
+void draw_artificial_horizon(float pitch, float roll);
 void draw_airspeed_indicator();
 void draw_heading_indicator();
 void draw_altitude_indicator();
