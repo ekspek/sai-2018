7eb039ceca2897556439e90f94d376a8e02ae40b
stalone89 <pedro.afonso_trf@hotmail.com>
2019-01-18 19:18:43 +0000

    Heading-bussola

--- project/draw_functions.c
+++ project/draw_functions.c
@@ -503,3 +503,128 @@ void draw_text(char* string, int x, int y, int angle, int scale){
 		aux++;
 	}
 }
+
+
+
+
+void drawCircle (GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides)
+{
+ GLint numberOfVertices = numberOfSides + 2;
+ GLfloat doublePi = 2.0f * PI;
+ GLfloat circleVerticesX[numberOfVertices];
+ GLfloat circleVerticesY[numberOfVertices];
+ GLfloat circleVerticesZ[numberOfVertices];
+
+ circleVerticesX[0]=x;
+ circleVerticesY[0]=y;
+ circleVerticesZ[0]=z;
+
+
+for (int i = 1; i < numberOfVertices; i++)
+    {
+    circleVerticesX[i] = x + (radius*cos(i + doublePi/numberOfSides));
+    circleVerticesY[i] = y + (radius*sin(i + doublePi/numberOfSides));
+    circleVerticesZ[i] = z;
+    }
+
+GLfloat allCircleVertices[numberOfVertices * 3];
+
+for(int i = 0; i< numberOfVertices; i++)
+    {
+    allCircleVertices[i*3] = circleVerticesX[i];
+    allCircleVertices[(i*3)+1] = circleVerticesY[i];
+    allCircleVertices[(i*3)+2] = circleVerticesZ[i];
+    }
+
+glEnableClientState( GL_VERTEX_ARRAY);
+glVertexPointer(3, GL_FLOAT, 0, allCircleVertices);
+glDrawArrays(GL_TRIANGLE_FAN, 0, numberOfVertices);
+glDisableClientState(GL_VERTEX_ARRAY);
+
+
+}
+
+
+
+void draw_heading_indicator(float heading)
+{
+
+    int i=0; //Auxiliary counter
+
+    float midX = (float) WINDOW_SIZE_X/2;
+
+    glMatrixMode(GL_MODELVIEW);
+
+    glLoadIdentity();
+    glColor3f (0.3 , 0.4 , 0.1) ;
+    drawCircle(400,850, 1, 200, 500);
+
+
+    glLoadIdentity();
+    glTranslatef(midX,640,1.5);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1,1,1);
+    glVertex3f(-10,0,0);
+    glVertex3f(10,0,0);
+    glVertex3f(0,15,0);
+    glEnd();
+
+
+
+
+    for (i=0;i<=359;i=i+10)
+    {
+            glLoadIdentity();
+            glTranslatef(midX, 850,2);
+            glRotatef(heading,0,0,1);
+            glRotatef(i,0,0,2);
+            glTranslatef(0, -200,0); //Move the matrix to the arc center
+            glBegin(GL_POLYGON);
+            glColor3f(1,1,1);
+            glVertex3f(-1,20,0);
+            glVertex3f(1,20,0);
+            glVertex3f(1,0,0);
+            glVertex3f(-1,0,0);
+            glEnd();
+        }
+
+
+    for (i=5;i<=359;i=i+10)
+    {
+            glLoadIdentity();
+            glTranslatef(midX, 850,2);
+            glRotatef(heading,0,0,1);
+            glRotatef(i,0,0,2);
+            glTranslatef(0, -200,0); //Move the matrix to the arc center
+            glBegin(GL_POLYGON);
+            glColor3f(1,1,1);
+            glVertex3f(-1,10,0);
+            glVertex3f(1,10,0);
+            glVertex3f(1,0,0);
+            glVertex3f(-1,0,0);
+            glEnd();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
