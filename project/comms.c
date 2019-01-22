243360e9ad916d767934746d8a98ca59f9dd74f7
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-01-22 18:17:28 +0000

    First version working, detects incoming message

--- project/comms.c
+++ project/comms.c
@@ -28,55 +28,51 @@ void* thread_comms(void* ptr){
 	struct sockaddr_in server_addr, client_addr;
 	//int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	//char buffer[BUFFER_SIZE];
-	//size_t bytes_in;
+    // Setup UDP socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0){
+        fprintf(stderr, "Error starting socket\n");
+        exit(1);
+    }
 
-	//int clientlen = sizeof(client_addr);
+    // Port Reusing
+    int optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,sizeof(int));
 
+    // Configure port
+    bzero((char *)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(*(uint32_t*)ptr);
 
-//    // Setup UDP socket
-//    struct sockaddr_in server_addr, client_addr;
-//    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-//    if(sockfd < 0){
-//        fprintf(stderr, "Error starting socket\n");
-//        exit(1);
-//    }
-//
-//    // Port Reusing
-//    int optval = 1;
-//    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,
-//                                                                   sizeof(int));
-//
-//    // Configure port
-//    bzero((char *)&serveraddr, sizeof(serveraddr));
-//    serveraddr.sin_family = AF_INET;
-//    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-//    serveraddr.sin_port = htons(*(uint32_t*)ptr);
-//
-//    // Bind to given port
-//    if(bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) != 0){
-//        perror("bind");
-//        exit(1);
-//    }
+    // Bind to given port
+    if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0){
+        perror("bind");
+        exit(1);
+    }
 
+	char buffer[BUFFER_SIZE];
+	size_t bytes_in;
 
+	int clientlen = sizeof(client_addr);
 
 	/* Main incoming data loop */
 	while(1){
-		bytes_in = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, (socklen_t*)&clientlen);
+		bytes_in = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, (socklen_t*)&clientlen);
 		if(bytes_in)
 			printf("Received message %s\n", buffer);
 	}
 
 	/* Mutex testing */
-	pthread_mutex_lock(&m);
-	sleep(3);
-	data_current.altitude = 2000;
-	printf("O valor da altitude é %f\n", data_current.altitude);
-	pthread_mutex_unlock(&m);
+	//pthread_mutex_lock(&m);
+	//sleep(3);
+	//data_current.altitude = 2000;
+	//printf("O valor da altitude é %f\n", data_current.altitude);
+	//pthread_mutex_unlock(&m);
 }
 
 int main (){
+	uint32_t port = 8500;
 	pthread_t thread_comms_id;
 
 	/* Initial values for the data structure */
@@ -88,7 +84,7 @@ int main (){
 	data_current.heading = 0;
 
 	/* Begin of communications thread */
-	pthread_create(&thread_comms_id,NULL,thread_comms,NULL);
+	pthread_create(&thread_comms_id,NULL,thread_comms,&port);
 
 	/* Mutex testing */
 	//sleep(1);
