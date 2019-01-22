51088761ff744261f954ff24ba60a03c62ce59d5
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-01-22 22:14:44 +0000

    Prepared comms.c code for merging with the main program

--- project/comms.c
+++ project/comms.c
@@ -20,19 +20,19 @@
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 Data data_current;
 
-// Reverses endianess of a floating point value
+/* Reverses endianess of a floating point value */
 float float_swap(float value){
-    union v {
-        float       f;
-        unsigned int    i;
-    };
-     
-    union v val;
-     
-    val.f = value;
-    val.i = htonl(val.i);
-                   
-    return val.f;
+	union v {
+		float f;
+		unsigned int i;
+	};
+	 
+	union v val;
+	 
+	val.f = value;
+	val.i = htonl(val.i);
+	               
+	return val.f;
 }
 
 /* Function that contains the code run by the communications thread */
@@ -41,58 +41,55 @@ void* thread_comms(void* ptr){
 
 	/* Setting up sockets and communications */
 	struct sockaddr_in server_addr, client_addr;
-	//int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    // Setup UDP socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sockfd < 0){
-        fprintf(stderr, "Error starting socket\n");
-        exit(1);
-    }
+	// Setup UDP socket
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd < 0){
+	    fprintf(stderr, "Error starting socket\n");
+	    exit(1);
+	}
 
-    // Port Reusing
-    int optval = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,sizeof(int));
+	// Port Reusing
+	int optval = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,sizeof(int));
 
-    // Configure port
-    bzero((char *)&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(*(uint32_t*)ptr);
+	// Configure port
+	bzero((char *)&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(*(uint32_t*)ptr);
 
-    // Bind to given port
-    if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0){
-        perror("bind");
-        exit(1);
-    }
+	// Bind to given port
+	if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0){
+	    perror("bind");
+	    exit(1);
+	}
 
 	char buffer[BUFFER_SIZE];
 	size_t bytes_in;
 
 	int clientlen = sizeof(client_addr);
 
-    // Sender's API places each variable on a given position
-    const int latitude_idx = 0;
-    const int longitude_idx = 4;
-    const int altitude_idx = 8;
-	double latitude;
-	double longitude;
-	double altitude;
+	// Sender's API places each variable on a given position
+	const int wordsize = 4;
 
 	/* Main incoming data loop */
 	while(1){
 		bytes_in = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, (socklen_t*)&clientlen);
-		if(bytes_in)
-			//printf("Received %ld bytes, the message is %s\n", bytes_in, buffer);
-            buffer[bytes_in]=0;
 
-            pthread_mutex_lock(&m);
-            latitude = float_swap(*(float*)(buffer+latitude_idx));
-            longitude = *(float*)(buffer+longitude_idx);
-            altitude =  *(float*)(buffer+altitude_idx);
-            //ready = 1;
-            pthread_mutex_unlock(&m);
-			printf("Data received, latitude is %f, longitude is %f, altitude is %f\n", latitude, longitude, altitude);
+		if(bytes_in){
+			pthread_mutex_lock(&m);
+			/* float_swap switches endians (program receives big-endian) */
+			data_current.altitude = float_swap(*(float*)(buffer + 0 * wordsize));
+			data_current.ias = float_swap(*(float*)(buffer + 1 * wordsize));
+			data_current.vspeed =  float_swap((*(float*)(buffer + 2 * wordsize)));
+			data_current.pitch =  float_swap((*(float*)(buffer + 3 * wordsize)));
+			data_current.roll =  float_swap((*(float*)(buffer + 4 * wordsize)));
+			data_current.heading =  float_swap((*(float*)(buffer + 5 * wordsize)));
+			pthread_mutex_unlock(&m);
+
+			printf("Altitude is %f, IAS is %f, vertical speed is %f, pitch is %f, roll is %f, heading is %f\n", data_current.altitude, data_current.ias, data_current.vspeed, data_current.pitch, data_current.roll, data_current.heading);
+		}
 	}
 }
 
